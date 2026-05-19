d038d97bfd10efe40ba451091fefd85261cd52e3
adidev001 <adidevrai001@gmail.com>
2026-05-19 11:08:13 +0530

    chore: configure explicit CORS whitelist for Vite ports

--- server/index.js
+++ server/index.js
@@ -14,7 +14,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Mount Routes
